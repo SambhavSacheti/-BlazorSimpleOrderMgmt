cd95c30960fd043cdf411e5c549cd758ae5fa638
sambhav <15857531+SambhavSacheti@users.noreply.github.com>
2020-08-25 21:02:34 +0100

    WebApi url is now in client's appsettings.json

--- Client/OrderManagement.Client/Program.cs
+++ Client/OrderManagement.Client/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Radzen;
 using OrderManagement.UI.Services;
 
-
 namespace OrderManagement.Client
 {
     public class Program
@@ -15,7 +14,6 @@ namespace OrderManagement.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<OrderManagement.UI.App>("app");
-
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<DialogService>();
             builder.Services.AddScoped<CustomerService>();
